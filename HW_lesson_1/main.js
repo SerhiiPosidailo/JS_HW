4721c4b9317dc409f66d3b5b7298237adbf9f5cc
Notebook <zekserj@ukr.net>
2023-08-16 08:43:49 +0300

    HW 1

---
+++ HW_lesson_1/main.js
@@ -0,0 +1,57 @@
+let s1 = 'hello';
+console.log(s1)
+let s2 = 'owu'
+console.log(s2)
+let s3 = 'ua'
+console.log(s3)
+
+
+let n1 = 1
+console.log(n1)
+let n2 = 10
+console.log(n2)
+let n3 = -999
+console.log(n3)
+let n4 = 123
+console.log(n4)
+let n5 = 3.14
+console.log(n5)
+let n6 = 2.7
+console.log(n6)
+let n7 = 16
+console.log(n7)
+
+let b1 = true
+console.log(b1)
+let b2 = false
+console.log(b2)
+
+// ----------------------------------------------------------------
+
+let firstName = 'Serhii'
+let middleName = 'Serhiyovych'
+let lastName = 'Posidailo'
+
+let nameRes = `${lastName} ${firstName} ${middleName}`
+console.log(nameRes)
+
+// -------------------------------------------------------------------
+
+let a = 100
+console.log(typeof a)
+
+let b = '100'
+console.log(typeof b)
+
+let c = true
+console.log(typeof c)
+
+// ------------------------------------------------------------------------
+
+let promp_1 = prompt('your firstName')
+let promp_2 = prompt('your lastName')
+let promp_3 = prompt('your age')
+
+let prompRes = `name "${promp_1} ${promp_2}" you "${promp_3}" age.`
+
+console.log(prompRes)
