3a879ea6dda112964c9c1f276e9d75eef8818216
Notebook <zekserj@ukr.net>
2023-08-30 10:15:37 +0300

    HW_5_Tasks

--- HW_lesson_5/main.js
+++ HW_lesson_5/main.js
@@ -1,54 +1,43 @@
+// ВСЕ ЗРОБИТИ СТРІЛОЧНИМИ ФУНКЦІЯМИ!
 // - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
-function rectangleS(a, b) {
-    let s = a * b
-    console.log(`the area of the rectangle:`, s)
-    return s
+let fun_1 = (a, b) => {
+    return a * b
 }
-
-let resFunctionRectangleS = rectangleS(5, 12)
-console.log(resFunctionRectangleS)
+console.log(fun_1(5, 12))
 
 // - створити функцію яка обчислює та повертає площу кола з радіусом r
-function circles(r) {
-    let areaS = 3.14 * r ** 2
-    console.log(`area of a circle:`, areaS)
-    return areaS
+let fun_2 = (r) => {
+    return 3.14 * r ** 2
 }
 
-let resFunctionCircles = circles(15)
-console.log(resFunctionCircles)
+console.log(fun_2(15))
 
 // - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
-function cylinder(h, r) {
-    let s = 2 * 3.14 * r * (r + h)
-    console.log(`cylinder area`, s)
-    return s
+let fun_3 = (h, r) => {
+    return 2 * 3.14 * r * (r + h)
 }
 
-let resFunctionCylinder = cylinder(15, 12)
-console.log(resFunctionCylinder)
+console.log(fun_3(15, 12))
 
 // - створити функцію яка приймає масив та виводить кожен його елемент
 let newArre = [1, 3, 5, true, 'hello okten', false]
 
-function eachArrayElement(arr) {
+let fun_4 = (arr) => {
     for (let arrElement of arr) {
         console.log(arrElement)
     }
 }
 
-eachArrayElement(newArre)
+fun_4(newArre)
 
 // - створити функцію яка створює параграф з текстом. Текст задати через аргумент
-function creatingParagraph(textParagraph) {
+let fun_5 = (textParagraph) => {
     document.write(`<p>${textParagraph}</p>`)
 }
 
-creatingParagraph('hello okten')
-
+fun_5('hello okten')
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
-// через цикл
-function ulIl(text_Il) {
+let fun_6 = (text_Il) => {
     document.write(`<ul>`)
     for (let i = 0; i < 3; i++) {
         document.write(`<li>${text_Il}</li>`)
@@ -56,21 +45,9 @@ function ulIl(text_Il) {
     document.write(`</ul>`)
 }
 
-ulIl('hello Friday')
-// ----------------------------------------------------------------------------------------------------------
-// без циклу
-function ulIl_1(text_Il) {
-    document.write(`<ul>`)
-    document.write(`<li>${text_Il}</li>`)
-    document.write(`<li>${text_Il}</li>`)
-    document.write(`<li>${text_Il}</li>`)
-    document.write(`</ul>`)
-}
-
-ulIl_1('hello Friday')
-
+fun_6('hello Friday')
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
-function ulLi_2(text, num) {
+let fun_7 = (text, num) => {
     document.write(`<ul>`)
     for (let i = 0; i < num; i++) {
         document.write(`<li>${text}</li>`)
@@ -78,11 +55,9 @@ function ulLi_2(text, num) {
     document.write(`</ul>`)
 }
 
-ulLi_2('Friday', 5)
-
+fun_7('Friday', 5)
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
-
-function creatingLists(arr) {
+let fun_8 = (arr) => {
     document.write(`<ul>`)
     for (let arrElement of arr) {
         document.write(`<li>${arrElement}</li>`)
@@ -90,24 +65,24 @@ function creatingLists(arr) {
     document.write(`</ul>`)
 }
 
-let resFunctionCreatingLists = creatingLists([1, 3, true, false, 'hello', 'friday'])
-console.log(resFunctionCreatingLists)
-
+fun_8([1, 3, true, false, 'hello', 'friday'])
 // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
-let users = [{id: 1, name: 'Max', age: 22}, {id: 2, name: 'Dima', age: 21}, {id: 3, name: 'Vasia', age: 12}]
+let users = [
+    {id: 1, name: 'Max', age: 22},
+    {id: 2, name: 'Dima', age: 21},
+    {id: 3, name: 'Vasia', age: 12}
+]
 
-function usersArr(users) {
+let fun_9 = (users) => {
     for (let user of users) {
         document.write(`<div>id: ${user.id} name: ${user.name} age : ${user.age}</div>`)
     }
 }
 
-usersArr(users)
-
+fun_9(users)
 // - створити функцію яка повертає найменьше число з масиву
 let arrNum = [5, 2, 4, 6, 3, 7, 9]
-
-function minNumArr(someArr) {
+let fun_10 =(someArr) => {
     let res ;
     for (let Element of someArr) {
         if (res === undefined){
@@ -118,13 +93,11 @@ function minNumArr(someArr) {
     }
     return res
 }
-let resFunctionMinNumArr  = minNumArr(arrNum)
-console.log(resFunctionMinNumArr)
-
+console.log(fun_10(arrNum))
 // - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
 let arrNum_1 = [1, 2, 6, 8, 2, 1]
 
-function sumAllArr(arr) {
+let fun_11 = (arr)=> {
     let result = 0
     for (let arrElement of arr) {
         result += arrElement
@@ -132,28 +105,26 @@ function sumAllArr(arr) {
     return result
 }
 
-let resFunctionSumAllArr = sumAllArr(arrNum_1)
-console.log(resFunctionSumAllArr)
-
+console.log(fun_11((arrNum_1)))
 // - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
 // Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
-
 let someSwapArray = [11, 22, 33, 44, 55];
-
-function swap(arr, index1, index2) {
+let fun_12 = (arr, index1, index2) => {
     let one = arr[index1]
     arr[index1] = arr[index2]
     arr[index2] = one
     return arr
 }
 
-let resFunctionSwap = swap(someSwapArray, 0, 1)
-console.log(resFunctionSwap)
-
+console.log(fun_12(someSwapArray, 0 ,1))
 // - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 // Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
-
-function exchange(sumUAH, currencyValues, exchangeCurrency) {
+let price = [
+    {currency: 'USD', value: 40},
+    {currency: 'EUR', value: 42},
+    {currency: 'PLN', value: 8}
+]
+let fun_13 =(sumUAH, currencyValues, exchangeCurrency) => {
     for (let item of currencyValues) {
         if (item.currency === exchangeCurrency){
             return sumUAH / item.value
@@ -161,5 +132,4 @@ function exchange(sumUAH, currencyValues, exchangeCurrency) {
     }
 }
 
-let resFunctionExchange = exchange(10000, [{currency: 'USD', value: 40}, {currency: 'EUR', value: 42}, {currency: 'PLN', value: 8}], 'USD')
-console.log(resFunctionExchange)
+console.log(fun_13(10000, price, 'USD'))
